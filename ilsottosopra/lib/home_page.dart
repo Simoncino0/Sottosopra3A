1f7440585c99f1bc0cb1d52146aaf4831715f914
Simoncino0 <simostella06@gmail.com>
2023-04-22 11:02:18 +0200

    mario

--- ilsottosopra/lib/home_page.dart
+++ ilsottosopra/lib/home_page.dart
@@ -2,9 +2,9 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:ilsottosopra/news_detail.dart';
 import 'package:ilsottosopra/widgets/news.dart';
-
-import 'news_detail.dart';
+import 'package:ilsottosopra/widgets/video_player.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,32 +16,41 @@ class HomePage extends StatelessWidget {
       children: [
       
        SizedBox(
-        height: MediaQuery.of(context).size.height * 0.3 ,
-        child: CarouselSlider.builder(itemCount: 4, itemBuilder: (context, index, realIndex) {
-          return Container(child: Image.asset("lib/assets/fiori.jpg"),);
-        },
-        options: CarouselOptions(autoPlay: true, enlargeCenterPage: true),
-        ),
+        height:  MediaQuery.of(context).size.height * 0.3,
+         child: CarouselSlider.builder(itemCount: 4, itemBuilder: (context, index, realIndex) {
+           return Container(
+             child: Image.asset("lib/assets/fiori.jpg", width: 300,),
+           );
+              
+         }, 
+         options: CarouselOptions(
+          autoPlay: true,
+          enlargeCenterPage: true,
+       
+          ),
+          ),
+          
        ),
+       VideoApp(),
+
+       
         Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child:ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: 4,
-            itemBuilder: (context, index) {
-              return GestureDetector(onTap: (){
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => const NewsDetail()),
-                  );
-              },
-             child: News() );
-            },)
-          ),
+        child: ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            
+          return GestureDetector(onTap: () {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const NewsDetail()),
+  );
+}, child: News());
+        },)
+        ),
       ],
-   ), );
-
-    
-  } 
+    ));
+  }
 }
